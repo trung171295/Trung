e32cb62a130b14307da4e884ec36812f7d0204a3
Trung <Trungk39c@gmail.com>
2019-02-27 15:32:36 +0700

    T
    ver 27/2

---
+++ src/js/menu_mobile.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(
+    function ()
+    {
+        menuMobile ();
+    });
+    
+    function menuMobile ()
+    {
+        $("#pageslide").click(function(){
+            $("#wrapper-menu-mobile").css({left:0, opacity:0});
+            $("#wrapper-menu-mobile").animate({opacity:1},200);
+            $("#wrapper-menu-mobile .wrapper").animate({ right:"0"},500);
+            jQuery(document.body).css('overflow', 'hidden');
+            return false;
+        });
+    
+        $("#bt-fechar-menu").click(function(){
+            closeMenuMobile ();
+            return false;
+        });
+    
+        $(".block-menu-mobile").mousedown(function(){
+            closeMenuMobile ();
+        });
+    }
+    
+    function closeMenuMobile (){
+        $("#wrapper-menu-mobile .wrapper").animate({ right:"-100%"},300,function(){
+            $("#wrapper-menu-mobile").animate({opacity:0},200,function(){
+                $("#wrapper-menu-mobile").css({left:"100%", opacity:1});
+                jQuery(document.body).css('overflow', 'auto');
+            });
+        });
+    }
+    jQuery(document).ready(
+        function ()
+        {
+            menuMobile ();
+        });
+        
+        function menuMobile ()
+        {
+            $("#pageslide").click(function(){
+                $("#wrapper-menu-mobile").css({left:0, opacity:0});
+                $("#wrapper-menu-mobile").animate({opacity:1},200);
+                $("#wrapper-menu-mobile .wrapper").animate({ right:"0"},500);
+                jQuery(document.body).css('overflow', 'hidden');
+                return false;
+            });
+        
+            $("#bt-fechar-menu").click(function(){
+                closeMenuMobile ();
+                return false;
+            });
+        
+            $(".block-menu-mobile").mousedown(function(){
+                closeMenuMobile ();
+            });
+        }
+        
+        function closeMenuMobile (){
+            $("#wrapper-menu-mobile .wrapper").animate({ right:"-100%"},300,function(){
+                $("#wrapper-menu-mobile").animate({opacity:0},200,function(){
+                    $("#wrapper-menu-mobile").css({left:"100%", opacity:1});
+                    jQuery(document.body).css('overflow', 'auto');
+                });
+            });
+        }
+            
---
+++ src/js/mobile.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.list-first-item').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
+        autoplaySpeed: 2000,       
+    });
+  });
+      
